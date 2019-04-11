942330b821a000ad7eaf3b1c19cf2af2c114f3d0
Tsachi Kotek <tkotek@allot.com>
2019-04-11 14:53:57 +0300

    first commit

---
+++ backend/app.py
@@ -0,0 +1,30 @@
+#!flask/bin/python
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+@app.route('/')
+def hello_whale():
+    return render_template("whale_hello.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
